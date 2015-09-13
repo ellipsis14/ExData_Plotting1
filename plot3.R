70c5a2ca2a4264eee3cb9b66a0f74761be38e113
ellipsis14 <jagaran09@gmail.com>
2015-09-13 17:47:29 -0500

    Semi-final

---
+++ plot3.R
@@ -0,0 +1,29 @@
+
+# Plot 3. Data should be stored in the Current Working directory
+
+# Read Data
+data <- read.csv("household_power_consumption.txt", na.string="?", sep=";")
+
+# Extract needed dataset
+data <- data[(data$Date=="2/1/2007" | data$Date=="2/2/2007"),]
+
+# Combining Date and Time
+data$DateTime <- strptime(paste(data$Date, data$Time, sep=" "), 
+                          format="%m/%d/%Y %H:%M:%S")
+
+# Open png device
+png("plot3.png", width=480, height=480)
+
+# scatter plot with the suggested Legend and colring for the legend
+plot(data$DateTime, data$Sub_metering_1, type="l", 
+     xlab="", ylab="Energy sub metering")
+lines(data$DateTime, data$Sub_metering_2, col="red")
+lines(data$DateTime, data$Sub_metering_3, col="blue")
+legend("topright", col=c("black", "red", "blue"), 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lty=1)
+
+# Turn off the device
+dev.off()
+# To inform that file has been saved to which location on the device
+cat("Plot3.png is saved to", getwd())
